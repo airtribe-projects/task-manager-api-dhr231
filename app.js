44ff96ac93f39a578ca502cbcf77ce4d117d66e8
dhr231 <dhruvprashar231@gmail.com>
2025-11-25 00:57:33 +0530

    feat: understood the validation logic in POST and PUT method

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/tasks', (req, res) => {
-    res.json(tasks);
+app.get('/tasks', (req, res) => { //using this method to get the complete list of tasks. using GET method for it. first argument is the route name which is /tasks. then there is callback function passed as an argument which tells the get method what to do when tasks route is passed in URL.
+    res.json(tasks); //this is the task that GET method does when route is /tasks. this line signifies that response of this method will tasks in form of JSON. Now what i need to understand it what are req and res like why is the syntax like this
 });
 
 app.get('/tasks/:id', (req, res) => {
@@ -33,8 +33,8 @@ app.post('/tasks', (req, res) => {
 
     const { title, description, completed } = req.body;
 
-    if (!title || typeof completed !== 'boolean') {
-        return res.status(400).json({
+    if (!title || typeof completed !== 'boolean') { //this line of code signifies where the task has title and completed attribute available. !title means NOT(title) and typeof is keyword used to check the data type of a variable.
+        return res.status(400).json({ //if the above conditions are true then the block returns status code as 400 along with message to correct the request.
             message: 'Invalid request: Title is required and "completed" must be a boolean.'
         });
     }
@@ -44,7 +44,7 @@ app.post('/tasks', (req, res) => {
     const newTask = {
         id: newId,
         title: title,
-        description: description || "", 
+        description: description || "",
         completed: completed
     };
 
@@ -72,9 +72,9 @@ app.put('/tasks/:id', (req, res) => {
     }
 
     tasks[taskIndex] = {
-        ...tasks[taskIndex], 
-        ...updatedTaskData, 
-        id: taskId 
+        ...tasks[taskIndex],
+        ...updatedTaskData,
+        id: taskId
     };
 
     res.status(200).json(tasks[taskIndex]);
@@ -93,6 +93,15 @@ app.delete('/tasks/:id', (req, res) => {
 
     res.status(204).send();
 });
+
+// app.get('/tasks/:id', (req, res) => { //the main function of this method is to fetch the specific task based on ID passed in the URL by the user. app as suggested by gemini is like a robot and get is the like asking the question from him and getting answer or response from him. first argument it /tasks:id which is like the exact instruction i am giving to the robot upon which it goes to the second argument which is a function but i dont know why does it not have a name and why do we need a request in get function when I have provided the Id in the URL cant we just extract that ID and search the array to find whether task for that ID is presen or not?
+//     const taskId = parseInt(req.params.id); //we are extracting the task Id from the request here again same question why do we need a request when the Id passed is in the URL? ParseInt is a simple typecastig function which takes the Id as a string from the req and converts it to an int because the Id in tasks array is an Int as well. then we have params which i dont know the use of cant we just directly access req.id to fetch it?
+//     const task = tasks.find(t => t.id === taskId); //i have seen this function for the first time but from the looks and reading of it looks like a search function for an array where t is a element of an array like i as iterator in a for loop. where for each t we are checking if the id attribute is matching for the input taskId form the request. task variable contains that matched task containig taskId.
+//     if (!task) {
+//         return res.status(404).json({ message: 'Task not found' }); // looks like a simple validation to me to check whether task variable contains any value if not it indicates that Id passed in the URL is invalid.
+//     }
+//     res.json(task); //outputs the found task as json.
+// })
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
